92700ee23494407b683a4b3767316c78e039d6db
Luis Macena <luis.macena@outlook.com>
2020-09-11 17:23:43 -0300

    Project update

--- YAD109-Programação_Estruturada_e_Modular/Projetos/validador.c
+++ YAD109-Programação_Estruturada_e_Modular/Projetos/validador.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main(){
 
 	int cpf[11];
@@ -21,6 +23,19 @@ int main(){
 	} 
 	else{
 		printf("\nErro no primeiro verificador.");	
+	}
+	int soma1=0;
+	
+	for(i=0; i<10; i++){
+		soma1 = soma1 + cpf[i]*m2[i];
+	}
+	int resto1 = ((soma1*10) % 11)%10;
+  	
+	if(resto1 == cpf[10]){
+		printf("\nSegundo verificador valido.");
+	}
+	else{
+		printf("\nErro no segundo verificador.");
 	}
 return 0;
 }
